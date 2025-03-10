49ec810d20a9c4420ffc10a98ce908fdc3544afe
cachooo <70294001+cachooo@users.noreply.github.com>
2025-03-10 20:27:02 +0800

    Feat: Include wiki app urlpatterns with namespace in project

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("wiki.urls"))
+    path("wiki/", include("wiki.urls", namespace="wiki")),
 ]
