21edbecf86673c8dacba2b317f0946e3b14e4d3f
vfa59 <vittorioang9898@gmail.com>
2025-03-11 19:13:50 +0800

    Registered Post and PostCategory models in admin with search and filters

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, PostCategory
 
-# Register your models here.
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+    search_fields = ('name', )
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    search_fields = ('name', )
+    list_display = ('title', 'category', 'created_on',)
+    list_filter = ('category',)
+
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
