6047511445393329acc430334e4fd457ab5a96a0
Sticity <119797972+Sticity@users.noreply.github.com>
2025-03-11 14:11:08 +0800

    Updated 'urls.py' in merchstore
    Created the paths and connected them to .views

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from .views import item, item_list
 
+urlpatterns = [
+    path('merchstore/item/<int:id>/', item, name="detail_view"),
+    path('merchstore/items/', item_list, name="list_view"),
+]
+
+app_name = "merchstore"
