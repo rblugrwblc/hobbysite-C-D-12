bae32ae8e2a0f1d7c2f2b8356b306882309bf4d6
cachooo <70294001+cachooo@users.noreply.github.com>
2025-03-13 17:52:04 +0800

    Refactor: to match style of commissions

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Article
 
-class ArticleListView(ListView):
-    model = Article
-    templateName = "wiki/article_list.html"
-    contextObjectName = "articles"
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'wiki/article_list.html', {'articles': articles})
 
-class ArticleDetailView(DetailView):
-    model = Article
-    templateName = "wiki/article_detail.html"
-    contextObjectName = "article"
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'wiki/article_detail.html', {'article': article})
 
 def home(request): 
-    return HttpResponse("<h1>Welcome to the Wiki. </h1><p>Go to <a href='/wiki/articles/'>Articles</a></p>")
+    return HttpResponse("<h1>Welcome to the Wiki.</h1><p>Go to <a href='/wiki/articles/'>Articles</a></p>")
