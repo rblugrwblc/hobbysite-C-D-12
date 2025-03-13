b3551697fbed34feba6b8532bd3f4415fcc6ccd0
cachooo <70294001+cachooo@users.noreply.github.com>
2025-03-13 19:00:17 +0800

    Feat: forum urlpattern included

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("wiki/", include("wiki.urls", namespace="wiki")),
-    path('commissions/', include("commissions.urls"))
-    path('', include('forum.urls')),
+    path('commissions/', include("commissions.urls")),
+    path('forum/', include('forum.urls')),
 ]
