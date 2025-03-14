a0e80961dc824fbbdd1fe4a064f49f387bac0735
cachooo <70294001+cachooo@users.noreply.github.com>
2025-03-14 13:07:20 +0800

    Refactor: Match style of blog app

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'commissions',
     'forum',
-    'blog'
+    'blog',
     'wiki',
 ]
 
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
-from .models import Article
+from .models import Article, ArticleCategory
 
 def article_list(request):
+    categories = ArticleCategory.objects.all()
     articles = Article.objects.all()
-    return render(request, 'wiki/article_list.html', {'articles': articles})
+    return render(request, 'wiki/article_list.html', {'articles': articles,
+                                                      'categories':categories})
 
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
