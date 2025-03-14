ae98587979de8b714da6980fe419ac249be88747
Sticity <119797972+Sticity@users.noreply.github.com>
2025-03-14 21:13:24 +0800

    Updated urls.py in commissions

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -3,8 +3,8 @@ from .views import commission_list, commission_detail
 
 
 urlpatterns = [
-    path('list/', commission_list, name='commission_list'),
-    path('detail/<int:pk>/', commission_detail, name='commission_detail'),
+    path('list/', commission_list, name='list_view'),
+    path('detail/<int:pk>/', commission_detail, name='detail_view'),
 ]
 
 app_name = 'commissions'
