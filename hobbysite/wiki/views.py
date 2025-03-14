2dc5764390026c24a49f257e581c8b524198928b
Sticity <119797972+Sticity@users.noreply.github.com>
2025-03-14 21:07:26 +0800

    Updated views.py in wiki

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Article, ArticleCategory
 
-def article_list(request):
+def wiki_article_list(request):
     categories = ArticleCategory.objects.all()
     articles = Article.objects.all()
-    return render(request, 'article_list.html', {'articles': articles,
+    return render(request, 'wiki_article_list.html', {'articles': articles,
                                                       'categories':categories})
 
-def article_detail(request, pk):
+def wiki_article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
-    return render(request, 'article_detail.html', {'article': article})
+    return render(request, 'wiki_article_detail.html', {'article': article})
 
 def home(request): 
     return HttpResponse("<h1>Welcome to the Wiki.</h1><p>Go to <a href='/wiki/articles/'>Articles</a></p>")
