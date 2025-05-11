9b2bfd3c737f4ade9496895e3a6d5b7d00b020fe
PressedK <lightning.mckey@yahoo.com>
2025-05-12 06:00:22 +0800

    implemented article creation and editing
    created articleform for creation and edits
    implemented fbv for updating articles
    - only accessible by authors of article being edited. also allows for only specific parts to be edited
    - updates the updated_on parameter
    
    implemented article creation

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,8 +1,9 @@
 # appname/views.py
 from django.shortcuts import render,redirect
 from .models import Article, ArticleCategory
-from .forms import CommentForm
+from .forms import CommentForm, ArticleForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 @login_required
 def blog_article_list(request):
@@ -43,13 +44,49 @@ def blog_article_detail(request, article_id):
     return render(request, 'blog_article_detail.html', {'article':article,
                                                         'common_articles':common_author_articles,
                                                         'comments':comments,
-                                                        'form': comment_form,
+                                                        'comment_form': comment_form,
                                                         'owner': is_owner} )
 
-def blog_article_edit(request, article_id):
-    
-    return render(request, 'blog_article_edit.html')
-
+@login_required
 def blog_article_create(request):
-  
-    return render(request, 'blog_article_create.html')
+
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)  
+
+        if article_form.is_valid():
+            article = article_form.save(commit=False)
+            article.author = request.user.profile
+            article.save()
+            return redirect(article.get_absolute_url())
+    else:
+        article_form = ArticleForm()  
+    
+
+    return render(request, 'blog_article_create.html', {'article_form':article_form})
+
+@login_required
+def blog_article_edit(request, article_id):
+    article = Article.objects.get(id = article_id)
+
+    if request.method == 'POST':
+        if not hasattr(request.user, 'profile') or article.author != request.user.profile:
+            return redirect('blog:list_view')
+        
+        edit_form = ArticleForm(request.POST, request.FILES, instance=article)
+        if edit_form.is_valid():
+            article = edit_form.save(commit=False)
+            article.updated_on = timezone.now()
+            article.save()
+            return redirect(article.get_absolute_url())
+    else:                                                   
+        edit_form = ArticleForm(instance=article, initial={
+            'title': article.title,
+            'category': article.category,
+            'entry': article.entry,
+            'header_image': article.header_image
+        })
+        
+    return render(request, 'blog_article_edit.html', {'edit_form': edit_form})
+
+
+
