bdedfa1c643fd8cf6314fd24d1a0fe90eb1a5fa9
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 23:13:37 +0800

    Edited ALLOWED_HOSTS

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hobbysite-c-d-12.onrender.com'] 
 
 
 # Application definition
