175d7061337fbc97dad22dafd41c4d0c018b5cff
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 23:41:59 +0800

    Updated settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR/'media'
