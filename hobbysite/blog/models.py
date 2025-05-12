b761ab10c53a2427b27400b3f64902b019f8be8c
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 22:58:51 +0800

    Updated models.py in blog

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile 
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -14,6 +15,7 @@ class ArticleCategory(models.Model):
     
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='blog_articles')
     category = models.ForeignKey(ArticleCategory,
                                  null=True,
                                  blank=True, 
@@ -21,6 +23,7 @@ class Article(models.Model):
     entry = models.TextField() 
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    header_image = models.ImageField(upload_to='images/', null=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -29,5 +32,25 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:article_detail", args=[str(self.id)])
-   
+        return reverse("blog:detail_view", args=[str(self.id)])
+    
+class Comment(models.Model):
+    article = models.ForeignKey(Article, 
+                                null=True,
+                                blank=True,
+                                on_delete=models.CASCADE,
+                                related_name='comments')
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='blog_comments')
+    entry = models.TextField() 
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+class ArticleGallery(models.Model):
+    article_gallery = models.ImageField(upload_to='images/', null=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name = 'images')
+
+    def __str__(self):
+        return f'Image for {self.article.title}'
