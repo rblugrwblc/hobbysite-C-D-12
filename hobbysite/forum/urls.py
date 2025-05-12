1fa7951ac681e2a0a84cb0152a745b3396fc5811
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 23:03:36 +0800

    Updated forums app

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import thread_list, thread_detail, thread_create, thread_edit
 
 app_name = "forum"
 
 urlpatterns = [
-    path('threads/', post_list, name="list_view"),
-    path('thread/<int:post_id>/', post_detail, name="detail_view"),
+    path('threads/', thread_list, name="list_view"),
+    path('thread/<int:thread_id>/', thread_detail, name="detail_view"),
+    path('thread/add/', thread_create, name='create_view'),
+    path('thread/<int:thread_id>/edit/', thread_edit, name='edit_view'),
 ]
