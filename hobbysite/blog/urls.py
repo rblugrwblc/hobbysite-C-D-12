9c4aaf68725e94f31ea450c0cf507614f76b140b
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 22:59:38 +0800

    Updated urls.py in blog

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,9 +1,10 @@
-# <appname>/urls.py
 from django.urls import path
-from .views import blog_article_list, blog_article_detail
+from .views import blog_article_list, blog_article_detail, blog_article_create, blog_article_edit
 urlpatterns = [
 path('articles', blog_article_list, name='list_view'),
 path('article/<int:article_id>', blog_article_detail, name='detail_view'),
+path('article/add', blog_article_create, name='create_view'),
+path('article/<int:article_id>/edit', blog_article_edit, name='edit_view'),
 ]
 
 app_name = "blog"
