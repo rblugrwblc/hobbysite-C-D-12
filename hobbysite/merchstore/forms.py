d2cd382a9f62ec69a6b68d1144d52b60862e5562
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 17:24:35 +0800

    Added forms.py

---
+++ hobbysite/merchstore/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Product, Transaction
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'product_type',
+                  'description', 'price',
+                  'stock', 'status']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount']
