7178dc54771fbc55c898077f4fe9f1240d2da059
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 23:06:47 +0800

    Updated urls.py in hobbysite

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("wiki/", include("wiki.urls", namespace="wiki")),
     path('commissions/', include("commissions.urls")),
     path('forum/', include('forum.urls')),
     path('blog/', include('blog.urls', namespace="blog")),
     path('merchstore/', include("merchstore.urls")),
+    path('', include('user_management.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
