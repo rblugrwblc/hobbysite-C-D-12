1dc1143a165cc492b84146baf85864e987f7b373
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-12 23:02:34 +0800

    Updated commissions app

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import commission_list, commission_detail
+from .views import CommissionCreateView, CommissionListView, CommissionUpdateView, CommissionDetailView, JobView
 
 
 urlpatterns = [
-    path('list/', commission_list, name='list_view'),
-    path('detail/<int:pk>/', commission_detail, name='detail_view'),
+    path('list/', CommissionListView.as_view(), name='list_view'),
+    path('detail/<int:pk>/',CommissionDetailView.as_view(), name='detail_view',),
+    path('<int:pk>/edit/', CommissionUpdateView.as_view(), name='update_commission'),
+    path('add/', CommissionCreateView.as_view(), name='add_commission'),
+    path('job/<int:pk>', JobView.as_view(), name='job_view'),
 ]
 
 app_name = 'commissions'
