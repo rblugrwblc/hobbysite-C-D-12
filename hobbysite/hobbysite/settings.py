f165a8509e1360c0bfc081e5bcf0eda168e16e38
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-14 21:44:36 +0800

    settings.py - Debug to False, STATIC_ROOT correction

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hobbysite-c-d-12.onrender.com', '127.0.0.1']
 
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [
+    BASE_DIR/'static',
+]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR/'media'
