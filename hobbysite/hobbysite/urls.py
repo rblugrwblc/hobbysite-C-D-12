982853f349c791c09db4eb632cbccd123364c918
Sticity <119797972+Sticity@users.noreply.github.com>
2025-05-15 13:02:05 +0800

    Update urls.py

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -30,5 +30,7 @@ urlpatterns = [
     path('', include('user_management.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
